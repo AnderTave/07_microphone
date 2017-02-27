ad3eb1ca31c8c83e65e6d908177967e60e93c3ff
AnderTave <sig.taverna@gmail.com>
2017-02-27 11:04:29 +0100

    musical notes
    Musical Note appears on a certain volume

--- sketch.js
+++ sketch.js
@@ -24,19 +24,40 @@ function draw() {
 	
 	background(colorback[selector]);
 	var vol = mic.getLevel();
-		
+
+//	If the volume is not enought, re-map it (set a higher newMax).
+//	var newMax = 5;
+//  volume = map(volume,0,1,0,newMax);
 	
+	//eyes
 	fill('white');
 	noStroke();
 	ellipse(width/3, 200,  dimensionx_eye[selector],  dimensiony_eye[selector]);
 	ellipse((width/3)*2, 200, dimensionx_eye[selector],  dimensiony_eye[selector]);
+	//iris
 	fill('black');
 	ellipse(width/3, 200, 50, 50);
 	ellipse((width/3) * 2, 200, 50, 50);
+	
+	//mounth
 	fill('#FFC0CB');
 	ellipse(width/2, (height/4) * 3, 200, vol * 300);
 	
-}
+	
+	if (vol>0.15){
+		image(note,(width/2)+60,((height/4) * 3)-100);
+	}
+
+	if (vol>0.18){
+		image(note,(width/2)+85,((height/4) * 3)-75);
+	}
+	
+	if (vol>0.2){
+		image(note,(width/2)+110,((height/4) * 3)-125);
+	}	
+	
+	}
+
 
 function mousePressed () {
 	selector++;
